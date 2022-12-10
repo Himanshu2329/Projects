e969c304aaa9da354d4a16de428368e402da0def
Himanshu2329 <kumarhimanshu4390@gmail.com>
2022-12-10 18:36:00 +0530

    Jframe Created

---
+++ Notepad/FirstSession.java
@@ -0,0 +1,14 @@
+package Notepad;
+
+import javax.swing.JFrame;
+
+/**
+ * FirstSession
+ */
+public class FirstSession {
+    static JFrame frame;
+    public static void main(String[] args) {
+        frame=new JFrame("First Session");
+        frame.setVisible(true);
+    }
+}
